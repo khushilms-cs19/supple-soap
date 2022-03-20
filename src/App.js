a93c1cbcb8807b79b8c797f5c252bb6e1fda6eba
khushilms-cs19 <khushilms.cs19@bmsce.ac.in>
2022-03-20 19:41:07 +0530

    added the home footer element

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ExperienceSupple from './components/Home/ExperieceSupple/ExperienceSupple
 import Navbar from './components/Navbar/Navbar';
 import About from './components/Home/About/About';
 import ContactUs from './components/Home/ContactUs/ContactUs';
+import HomeFooter from './components/Home/Footer/Footer';
 function App() {
 
     return (
@@ -13,6 +14,7 @@ function App() {
             <BestSellers />
             <About />
             <ContactUs />
+            <HomeFooter />
         </div>
     );
 }
